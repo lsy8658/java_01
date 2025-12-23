20017d5ba57eb65543052384d72d9945a00f7d68
lsy8658 <socszz@gmail.com>
2025-12-23 21:02:51 +0900

    java 01

---
+++ java/sample/src/단축키/단축키.java
@@ -0,0 +1,16 @@
+package 단축키;
+
+public class 단축키 {
+
+}
+
+/*
+  한 줄 복사 ctrl + alt + up/down key
+  코드이동 alt + up/down key
+  한 줄 삭제 ctrl + D
+  새 파일 생성 ctrl + N
+  탭 종료 ctrl + W
+  전체 탭 종료 ctrl +  shift + W
+   프로그램 실행 ctrl + f11
+   
+*/
